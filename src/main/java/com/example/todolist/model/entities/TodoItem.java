b1ea48e599665b868702fa20fff9d57597ebf18f
josezaq <jose.zaq@github.com>
2021-11-10 19:28:20 +0100

    conection with postgres fixed, aplication.properties modified

--- src/main/java/com/example/todolist/model/entities/TodoItem.java
+++ src/main/java/com/example/todolist/model/entities/TodoItem.java
@@ -4,13 +4,14 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
 @Entity
 public class TodoItem {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // strategia declarada por errores en postgres
     private long idItem;
     private String descripcio;
     private boolean fet = false;
