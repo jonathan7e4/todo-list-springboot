e84b53cf1a0c9662b8499a49c9b88c29d7a8945a
jonathan.navarro.7e4 <jonathan.navarro.7e4@itb.cat>
2021-11-22 09:34:21 +0100

    WebController.java && ItemListService.java updated

--- src/main/java/com/example/todolist/controllers/WebController.java
+++ src/main/java/com/example/todolist/controllers/WebController.java
@@ -44,16 +44,7 @@ public class WebController {
         if ( todoItem == null ) return ResponseEntity.notFound().build();
         else
         {
-            List<TodoItem> oldListTasks = itemListService.consultarItemsLlista( listId );
-
             ItemList itemList = itemListService.consultarLlista( listId );
-
-            this.eliminarLlista( listId );
-
-            itemListService.afegirLlista( itemList );
-
-            for ( TodoItem oldTask : oldListTasks ) if ( oldTask.getIdItem() != taskId ) todoItemService.afegirItem( oldTask );
-
             todoItem.setIdItem( taskId );
             if ( itemList != null )
             {
@@ -62,7 +53,7 @@ public class WebController {
             }
             //return ResponseEntity.ok( itemListService.updateTask( todoItem, listId, taskId ) );
         }
-        return ResponseEntity.ok( todoItemService.afegirItem( todoItem ) );
+        return ResponseEntity.ok( todoItem );
     }
 
 
