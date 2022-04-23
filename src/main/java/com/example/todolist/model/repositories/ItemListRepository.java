34d22daa90edc2e750ae52aea004311ca629eb7e
machine\cogrhyth <jonathan.navarro.7e4@itb.cat>
2022-04-23 18:06:48 +0200

    message

--- src/main/java/com/example/todolist/model/repositories/ItemListRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.todolist.model.repositories;
-
-import com.example.todolist.model.entities.ItemList;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemListRepository extends JpaRepository<ItemList, Integer> {
-}
--- src/main/java/com/example/todolist/model/repositories/TodoItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.todolist.model.repositories;
-
-import com.example.todolist.model.entities.TodoItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
-}
